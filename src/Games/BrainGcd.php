afc18305b9cd9571a1fb542777c7170802226808
Emit27 <46993682+Emit27@users.noreply.github.com>
2020-04-30 20:30:35 +1000

    Update

---
+++ src/Games/BrainGcd.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Braingames\Games\BrainCalc;
+
+use function Cli\line;
+use function Braingames\RunGame\startingGame as start;
+
+const MIN_RAND = 1;
+const MAX_RAND = 50;
+const NUMBER_QUESTIONS = 3;
+
+function text()
+{
+    return "What is the result of the expression? \n";
+}
+
+function run()
+{
+    $userData = [];
+    $userData[] = text();
+    for ($i = 1; $i <= NUMBER_QUESTIONS; $i++) {
+        $numberRandFirst = rand(MIN_RAND, MAX_RAND);
+        $numberRandSecond = rand(MIN_RAND, MAX_RAND);
+        $operator = array('*','+','-');
+        $randoperator = $operator[rand(0, 2)];
+        switch ($randoperator) {
+            case '*':
+                $operationResult = $numberRandFirst * $numberRandSecond;
+                break;
+            case '+':
+                $operationResult = $numberRandFirst + $numberRandSecond;
+                break;
+            case '-':
+                $operationResult = $numberRandFirst - $numberRandSecond;
+                break;
+        }
+
+        $operatExpression = "{$numberRandFirst} {$randoperator} {$numberRandSecond}";
+        $userData[] = [$operatExpression, $operationResult];
+    }
+    start($userData);
+}
