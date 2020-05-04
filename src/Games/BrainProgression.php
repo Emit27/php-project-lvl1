4751d6abd5f11451398d6434a884148f9744b0b4
Emit27 <46993682+Emit27@users.noreply.github.com>
2020-05-04 23:27:36 +1000

    create Progression

--- src/Games/BrainProgression.php
+++ src/Games/BrainProgression.php
@@ -1,25 +1,38 @@
 <?php
 
-namespace Braingames\Games\BrainGcd;
+namespace Braingames\Games\BrainProgression;
 
 use function Cli\line;
 use function Braingames\RunGame\startingGame as start;
 
-const MIN_RAND = 10;
-const MAX_RAND = 90;
+const MIN_RAND = 1;
+const MAX_RAND = 200;
+const MAX_PROGRESSION_LENGHT = 10;
 const NUMBER_QUESTIONS = 3;
+
+function createProgression()
+{
+    $increment = rand(3, 8);
+    $startProgression = rand(MIN_RAND, MAX_RAND);
+    do {
+        $startProgression--;
+        $endofProgression = $startProgression + ($increment * 9);
+    } while ($endofProgression > MAX_RAND);
+    $progressionNumbers = [];
+    $progressionNumbers[] = $startProgression;
+    for ($i = 1; $i < MAX_PROGRESSION_LENGHT; $i++) {
+        $progressionNumbers[] = $startProgression = $startProgression + $increment;
+    }
+    $ItemPosition = rand(1, 8);
+    $correctAnswer = $progressionNumbers[$ItemPosition];
+    $progressionNumbers[$ItemPosition] = '..';
+    $progressionDisplay = implode(" ", $progressionNumbers);
+    return [$progressionDisplay, $correctAnswer];
+}
 
 function text()
 {
-    return "Find the greatest common divisor of given numbers. \n";
-}
-
-function nodGcd($a, $b)
-{
-    if ($b == 0) {
-        return $a;
-    }
-    return nodGcd($b, $a % $b);
+    return "What number is missing in the progression? \n";
 }
 
 function run()
@@ -27,11 +40,7 @@ function run()
     $userData = [];
     $userData[] = text();
     for ($i = 1; $i <= NUMBER_QUESTIONS; $i++) {
-        $numberRandFirst = rand(MIN_RAND, MAX_RAND);
-        $numberRandSecond = rand(MIN_RAND, MAX_RAND);
-        $correctAnswer = nodGcd($numberRandFirst, $numberRandSecond);
-        $createNum = "{$numberRandFirst}  {$numberRandSecond}";
-        $userData[] = [$createNum, $correctAnswer];
+        $userData[] = createProgression();
     }
     start($userData);
 }
